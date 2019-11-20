567180c4cc17525722c9bd89b51b7c37c1acddcf
sandrusmb <sandrusmb@gmail.com>
2019-11-20 10:06:11 +0100

    Add js

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,12 +1,35 @@
 "use strict";
 
-// elemento de HTML
-const button = document.querySelector(".alert");
+// Botón de prueba con el que empieza todo
+const button = document.querySelector(".js-btn");
+const clue = document.querySelector(".js-clue");
+const message = document.querySelector(".js-tries");
 
-// handler
-function showAlert() {
-  console.log("Alerta");
+function getRandomNumber() {
+  const myRandomNumber = Math.floor(Math.random() * 100);
+  console.log("Este es mi número aleatorio: " + myRandomNumber);
+}
+
+function writeMessage() {
+  //Si el valor del input es igual al número aleatorio saldrá un mensaje en clue
+  const inputValue = document.querySelector("js-answer").value;
+  if (inputValue === myRandomNumber) {
+    clue.innerHTML("¡Enhorabuena campeona!");
+  } else if(inputValue > myRandomNumber){
+    clue.innerHTML("Muy alto");
+  } else(inputValue < myRandomNumber){
+    clue.innerHTML("Muy bajo");
+  } 
+}
+
+function buttonHandler() {
+  //generar un número aleatorio (HECHO)
+  getRandomNumber();
+  //pintar pistas
+  writeMessage();
+  //pintar número de intentos
+  writeNumberIntens();
 }
 
 // listener sobre el elemento, con tipo de evento y handler
-button.addEventListener("click", showAlert);
+button.addEventListener("click", buttonHandler);
