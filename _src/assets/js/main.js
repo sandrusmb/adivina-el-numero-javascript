7b9a19fbbf15149bf5b6e7dddd2f9f96e4cfa6b4
sandrusmb <sandrusmb@gmail.com>
2019-11-20 08:39:51 +0100

    Add html content

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,12 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+// elemento de HTML
+const button = document.querySelector(".alert");
+
+// handler
+function showAlert() {
+  console.log("Alerta");
+}
+
+// listener sobre el elemento, con tipo de evento y handler
+button.addEventListener("click", showAlert);
